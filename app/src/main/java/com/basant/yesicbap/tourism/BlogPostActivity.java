2716788f54e52c75aa54038c97a5128bccfd5571
Basant Bhandari <basantbhandari2074@gmail.com>
2019-11-10 20:20:13 +0545

    first commit

---
+++ app/src/main/java/com/basant/yesicbap/tourism/BlogPostActivity.java
@@ -0,0 +1,14 @@
+package com.basant.yesicbap.tourism;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BlogPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_post);
+    }
+}
